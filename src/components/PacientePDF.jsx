5c9df71555bd3c4df394bc4c42eb1a2da57404c1
germis22 <golpa22@gmail.com>
2022-09-01 05:25:39 -0400

    Se corrigio un error

--- src/components/PacientePDF.jsx
+++ src/components/PacientePDF.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
 
 const PacientePDF = (paciente) => {
 
